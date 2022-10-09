dde6deff6b3af73f3e702d7871d11c33f55edd9f
Matthieu <matthieu@pixel-developpement.com>
2022-10-09 09:45:16 +0200

    + Add strict_types

--- src/Constraints/CloudflareTurnstile.php
+++ src/Constraints/CloudflareTurnstile.php
@@ -1,5 +1,6 @@
 <?php
 
+declare(strict_types=1);
 namespace PixelDev\CloudflareTurnstileBundle\Constraints;
 
 use Symfony\Component\Validator\Constraint;
--- src/PixelDevCloudflareTurnstileBundle.php
+++ src/PixelDevCloudflareTurnstileBundle.php
@@ -1,5 +1,6 @@
 <?php
 
+declare(strict_types=1);
 namespace PixelDev\CloudflareTurnstileBundle;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
